c4e191e8f35c2952ccab53a6d8b7c0cdc3a4fe57
TeodorGot <54770127+TeodorGot@users.noreply.github.com>
2021-12-15 12:15:58 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,15 @@ import streamlit as st
 import pandas as pd
 st.write("Equity Mix Election")
 st.write("Plans")
-st.write(df=pd.DataFrame({
+st.write(pd.DataFrame({
     'RSU %': [100, 75, 50],
     'Option %': [0, 25, 50], 'Plan':['Plan 1','Plan 2', 'Plan 3']
 }))
+
+df = pd.DataFrame({
+    'RSU %': [100, 75, 50],
+    'Option %': [0, 25, 50], 'Plan':['Plan 1','Plan 2', 'Plan 3']
+})
 option = st.selectbox(
    'Which number do you like best?',
      df['Plan'])
