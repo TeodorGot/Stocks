ba8970daca9ff3b1eacffa6987f9b523a8cada2f
TeodorGot <54770127+TeodorGot@users.noreply.github.com>
2021-12-15 12:13:27 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,3 +15,14 @@ option = st.selectbox(
      df['Plan'])
 
 'You selected: ', option
+
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+    })
+
+option = st.selectbox(
+    'Which number do you like best?',
+     df['first column'])
+
+'You selected: ', option
