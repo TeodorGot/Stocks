d1bb563e9bf0c02f28ab196f2415101cd6d3d258
TeodorGot <54770127+TeodorGot@users.noreply.github.com>
2021-12-15 12:12:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,10 @@ st.write("Equity Mix Election")
 st.write("Plans")
 st.write(pd.DataFrame({
     'RSU %': [100, 75, 50],
-    'Option %': [0, 25, 50]
+    'Option %': [0, 25, 50], 'Plan':['Plan 1','Plan 2', 'Plan 3']
 }))
+option = st.selectbox(
+    'Which number do you like best?',
+     df['Plan'])
+
+'You selected: ', option
